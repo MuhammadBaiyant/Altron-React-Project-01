0c01d12929e6b8c95462ee8390811fafc779a9f4
MuhammadBaiyant <mbaiyant10@gmail.com>
2026-07-13 15:40:11 +0200

    tailwind updated

--- To-Do-App/src/components/Header.tsx
+++ To-Do-App/src/components/Header.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="bg-gray-800 p-4 flex justify-center items-center">
+      <h1 className="text-red-500 text-2xl font-bold border border-red-700">
+        Ta App
+      </h1>
+    </div>
+  );
 };
 
 export default Header;
