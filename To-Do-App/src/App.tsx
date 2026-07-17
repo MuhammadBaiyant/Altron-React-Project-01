f5c43eccd5605c6431dcc0d733b4b76a5e3f1c33
Samukelisiwe <ST10489024@rcconnect.edu.za>
2026-07-17 08:16:25 +0200

    footer and some testing changes

--- To-Do-App/src/App.tsx
+++ To-Do-App/src/App.tsx
@@ -3,10 +3,18 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <div className="flex flex-col h-screen w-screen bg-yellow-500">
+      <div className="flex-1">
+        <Header />
+      </div>
+      <div>
+        Bodyyyyy tea
+      </div>
+      <div className="">
+        <Footer />
+      </div>
+
+    </div>
   );
 }
 
